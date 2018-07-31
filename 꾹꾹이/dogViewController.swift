272b83c09fe1869c56a20a15a010ad260596177e
KoHoYun <ghdbs4959@naver.com>
2018-07-31 18:10:08 +0900

    dog
    dog까지

--- 꾹꾹이/dogViewController.swift
+++ 꾹꾹이/dogViewController.swift
@@ -8,28 +8,103 @@
 
 import UIKit
 
-class dogViewController: UIViewController {
 
+class dogViewController: UIViewController {
+    
+    
+    
+    
+    @IBOutlet weak var boneImage: UIImageView!
+    
+    @IBOutlet weak var dogImage: UIImageView!
+    
+ 
+    @IBOutlet weak var leftButton: UIButton!
+   
+    @IBOutlet weak var rightButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        dogImage.image = UIImage(named: "standard")!
+        boneImage.image = UIImage(named:"bone")
+        
         // Do any additional setup after loading the view.
     }
-
+    
+    var leftUp = false
+    var rightUp = false
+    
+    @IBAction func clickLeft(_ sender: UIButton) {
+        
+        //귀모양 사진
+        if(leftUp == true && rightUp == true){
+            dogImage.image = UIImage(named: "right")!
+        }
+        else if(leftUp == false && rightUp == true){
+            dogImage.image = UIImage(named: "merong")!
+            
+        }
+        else if(leftUp == false && rightUp == false){
+            dogImage.image = UIImage(named: "left")!
+            
+        }
+        else {
+            dogImage.image = UIImage(named: "standard")!
+        }
+        
+        if(leftUp == false){
+            leftUp = true
+        }
+        else{
+            leftUp = false
+        }
+        
+        
+    }
+    
+    @IBAction func clickRight(_ sender: Any) {
+        
+        if(leftUp == true && rightUp == true){
+            dogImage.image = UIImage(named: "left")!
+        }
+        else if(leftUp == false && rightUp == true){
+            dogImage.image = UIImage(named: "standard")!
+            
+        }
+        else if(leftUp == false && rightUp == false){
+            dogImage.image = UIImage(named: "right")!
+            
+        }
+        else {
+            dogImage.image = UIImage(named: "merong")!
+        }
+        
+        if(rightUp == false){
+            rightUp = true
+        }
+        else{
+            rightUp = false
+        }
+        
+        
+        
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
--- 꾹꾹이/ppocViewController.swift
+++ 꾹꾹이/ppocViewController.swift
@@ -8,32 +8,60 @@
 
 import UIKit
 
-class bubble {
+class Bubble {
     var bubble:UIButton
-    var bubble 
+    //var bubbleImage:UIImage = UIImage(named: "ppoc")!
+    var isClicked = false
+    
+    init(bubble:UIButton) {
+        self.bubble = bubble
+        setBubbleImage()
+    }
+    func setBubbleImage(){
+        bubble.imageView?.image = UIImage(named: "ppoc")
+    }
+    
+    func whenTouchBubble() {
+        if(isClicked == false){
+            bubble.imageView?.image = UIImage(named: "pooc2")
+        }
+    }
 }
-class ppocViewController: UIViewController {
 
+
+class ppocViewController: UIViewController {
+    
+    //뽁뽁이를 눌렀을때
+    @IBAction func clickBubble(_ sender: UIButton) {
+        sender.setImage(UIImage(named: "ppoc4"), for: .normal)
+        sender.isEnabled = false
+    }
+    
+    //리플레이를 눌렀을때
+    @IBAction func clickReplay(_ sender: UIBarButtonItem) {
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
